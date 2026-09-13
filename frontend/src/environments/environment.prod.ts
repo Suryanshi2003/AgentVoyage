131b77dbb63505f5a803712eeeec168d103b6d91
Suryanshi <suryanshi697@gmail.com>
2026-09-14 02:22:18 +0530

    update api url in env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://your-backend-name.onrender.com/api/travel'
+    apiUrl: 'https://agentvoyage-i0ga.onrender.com'
 };
